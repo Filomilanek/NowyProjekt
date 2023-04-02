1312d73f8b11be0c9f4ddbe07eca04405dbdb923
Bartłomiej Kowalski <Filomilanek@gmail.com>
2023-04-02 16:00:45 +0200

    First Commit

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
 
 public class UltimateQATest extends PageSetUp {
 
@@ -41,6 +42,30 @@ public class UltimateQATest extends PageSetUp {
         buttonSuccess = driver.findElement(By.className("entry-title"));
         Assertions.assertEquals( "Button success", buttonSuccess.getText());
 
+    }
+
+    @Test
+    public void testFour() throws InterruptedException {
+        WebElement nameInputID;
+        WebElement nameInputemail;
+        WebElement enter;
+        WebElement buttonSuccess;
+
+        nameInputID = driver.findElement(By.id("et_pb_contact_name_0"));
+        nameInputID.sendKeys("Tester");
+
+        nameInputemail = driver.findElement(By.id("et_pb_contact_email_0"));
+        nameInputemail.sendKeys("tester@tester.pl");
+
+        Thread.sleep(2000);
+
+        enter = driver.findElement(By.name("et_builder_submit_button"));
+        enter.click();
+
+        Thread.sleep(2000);
+
+        buttonSuccess = driver.findElement(By.id("et_pb_contact_form_0"));
+        Assertions.assertEquals( "Thanks for contacting us", buttonSuccess.getText());
     }
 
 
